359e98311ecda0d8fc4c8e7fb3232b6873f61b09
Austin Aurelio <austin@delveam.com>
2022-12-02 00:05:02 -0600

    Solve day 2 part 2

--- src/aoc_2022_clj/day02.clj
+++ src/aoc_2022_clj/day02.clj
@@ -1,9 +1,9 @@
 (ns aoc-2022-clj.day02
   (:require [clojure.string :as str]))
 
-(def rock 1)
-(def paper 2)
-(def scissors 3)
+(def rock 0)
+(def paper 3)
+(def scissors 6)
 
 (defn sanitize
   [input]
@@ -17,22 +17,22 @@
        (map #(str "(" % ")"))
        (map read-string)))
 
-(defn get-outcome-score
+(defn get-choice-score
   [round]
   (cond (= round '(rock rock)) 3
-        (= round '(rock paper)) 6
-        (= round '(rock scissors)) 0
-        (= round '(paper rock)) 0
-        (= round '(paper paper)) 3
-        (= round '(paper scissors)) 6
-        (= round '(scissors rock)) 6
-        (= round '(scissors paper)) 0
-        (= round '(scissors scissors)) 3))
+        (= round '(rock paper)) 1
+        (= round '(rock scissors)) 2
+        (= round '(paper rock)) 1
+        (= round '(paper paper)) 2
+        (= round '(paper scissors)) 3
+        (= round '(scissors rock)) 2
+        (= round '(scissors paper)) 3
+        (= round '(scissors scissors)) 1))
 
 (defn solve
   [rounds]
   (let [score1 (map #(eval (second %)) rounds)
-        score2 (map get-outcome-score rounds)]
+        score2 (map get-choice-score rounds)]
     (reduce + (map + score1 score2))))
 
 (print (solve (parse (sanitize (slurp "input/day02.txt")))))
