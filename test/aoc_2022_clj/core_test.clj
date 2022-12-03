d8fcc188620d875ba15d671efe966281eb6a3293
Austin Aurelio <austin@delveam.com>
2022-12-03 00:23:34 -0600

    Clean up project

--- test/aoc_2022_clj/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns aoc-2022-clj.core-test
-  (:require [clojure.test :refer :all]
-            [aoc-2022-clj.day01 :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
