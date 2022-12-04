e435f8d23e02c865a3aa583724f97841e06ee040
Austin Aurelio <austin@delveam.com>
2022-12-03 23:45:34 -0600

    Solve day 4 part 1

---
+++ src/aoc_2022_clj/day04.clj
@@ -0,0 +1,25 @@
+(ns aoc_2022_clj.day04
+  (:require
+    [clojure.set :as set]
+    [clojure.string :as str]))
+
+(defn expand-range [raw-spec] ; input ex: "2-4"
+  (let [spec (read-string (str "[" (str/replace raw-spec "-" " ") "]"))
+        lower (get spec 0)
+        upper (+ 1 (get spec 1))]
+    (set (range lower upper))))
+
+(defn range-contains? [range1 range2]
+  (let [r1 (expand-range range1) r2 (expand-range range2) intersect (set/intersection r1 r2)]
+    (or (= intersect r1) (= intersect r2))))
+
+(defn parse [input]
+  (->> (str/split-lines input)
+       (map #(str/split % #","))
+       (map #(apply range-contains? %))
+       (filter true?)
+       (count)))
+
+(->> (slurp "input/day04.txt")
+     (parse)
+     (print))
