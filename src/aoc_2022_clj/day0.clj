a1e71bb821014ab0dc9436dd08950bbd74f4980f
Austin Aurelio <austin@delveam.com>
2022-12-06 20:31:28 -0600

    Solve day 6 part 2

--- src/aoc_2022_clj/day0.clj
+++ src/aoc_2022_clj/day0.clj
@@ -1,8 +1,8 @@
 (ns aoc-2022-clj.day0)
 
 (defn solve [input]
-  (->> (partition 4 1 input)
+  (->> (partition 14 1 input)
        (map-indexed vector)
        (filter #(= (count (set (second %))) (count (second %))))))
 
-(print (+ 4 (first (first (solve (slurp "input/day06.txt"))))))
+(print (+ 14 (first (first (solve (slurp "input/day06.txt"))))))
