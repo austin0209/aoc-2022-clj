7ef05fdf59904d32d85aee5722f73d4fe0decddd
Austin Aurelio <austin@delveam.com>
2022-12-11 00:45:48 -0600

    Solve day 8 part 2

--- src/aoc_2022_clj/day08.clj
+++ src/aoc_2022_clj/day08.clj
@@ -17,23 +17,24 @@
        (map #(- (int %) (int \0)))
        (vec)))
 
-(defn is-visible-dir [val pos dir trees]
+(defn view-distance [val pos dir trees]
   (let [next-pos (map + pos dir)
         next (apply get-pos (cons trees next-pos))]
     (cond
-      (nil? next) true
-      (< next val) (is-visible-dir val next-pos dir trees)
-      :else false)))
+      (nil? next) 0
+      (< next val) (+ 1 (view-distance val next-pos dir trees))
+      :else 1)))
 
 (defn solve [trees]
   (->> (for [x (range map-w) y (range map-h)] [x y])
+       (filter #(and (> (first %) 0) (< (first %) map-w)
+                     (> (second %) 0) (< (second %) map-h)))
        (map #(let [v (apply get-pos (cons trees %))]
-               (or (is-visible-dir v % '(0 1) trees)
-                   (is-visible-dir v % '(0 -1) trees)
-                   (is-visible-dir v % '(1 0) trees)
-                   (is-visible-dir v % '(-1 0) trees))))
-       (filter true?)
-       (count)))
+               (* (view-distance v % '(0 1) trees)
+                  (view-distance v % '(0 -1) trees)
+                  (view-distance v % '(1 0) trees)
+                  (view-distance v % '(-1 0) trees))))
+       (apply max)))
 
 (def trees (parse "input/day08.txt"))
 (print (solve trees))
