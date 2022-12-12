7502e065fc6d65189f9a31beb3381233c8c32ae9
Austin Aurelio <austin@delveam.com>
2022-12-11 23:28:56 -0600

    Solve day 9 part 1

--- src/aoc_2022_clj/day09.clj
+++ src/aoc_2022_clj/day09.clj
@@ -1,18 +1,25 @@
 (ns aoc-2022-clj.day09
   (:require [clojure.string :as str]))
 
+(defn move-tail? [p1 p2]
+  (let [a (Math/abs ^int (- (first p1) (first p2)))
+        b (Math/abs ^int (- (second p1) (second p2)))]
+    (or (> a 1) (> b 1))))
+
 (defn simulate [init-state [dir distance]]
-  (loop [{:keys [pos visited] :as state} init-state
-         to-move distance
-         ignore-last (:ignore-last init-state)]
+  (loop [{:keys [pos tail-pos visited] :as state} init-state
+         to-move distance]
     (if (= to-move 0)
-      (assoc state :ignore-last true)
+      state
       (let [new-pos (map + pos dir)
-            new-visited (conj visited new-pos)
-            new-state (if (and (= to-move 1) ignore-last) (assoc state :pos new-pos)
-                                       (-> (assoc state :pos new-pos)
-                                           (assoc :visited new-visited)))]
-        (recur new-state (- to-move 1) ignore-last)))))
+            new-tail-pos (if (move-tail? new-pos tail-pos)
+                           pos
+                           tail-pos)
+            new-visited (conj visited new-tail-pos)
+            new-state (-> (assoc state :pos new-pos)
+                          (assoc :tail-pos new-tail-pos)
+                          (assoc :visited new-visited))]
+        (recur new-state (- to-move 1))))))
 
 (defn parse [input]
   (-> (str/replace input "R" "(1 0)")
@@ -23,13 +30,13 @@
            (map #(str "[" % "]"))
            (map #(read-string %)))))
 
-;; TODO: ignoring last is not correct at all! sometimes you need to ignore more than the last. see lines 6 and 7
-;; in the example. it's probably better to actually keep track of both head and tail position within the state.
-
-(def start {:pos '(0 0), :visited #{'(0, 0)}, :ignore-last true})
+(def start {:pos '(0 0), :tail-pos '(0 0), :visited #{'(0, 0)}})
 (def end (reduce simulate start (parse (slurp "input/day09.txt"))))
 (println (count (:visited end)))
+
 (sort-by #(str (second %) (first %)) (map vec (apply vector (:visited end))))
-(-> (simulate {:pos '(0 0) :visited #{'(0, 0)} :ignore-last true} ['(1 0) 3])
-    (simulate ['(0 1) 2])
-    (simulate ['(-1 0) 3]))
+
+;(-> (simulate {:pos '(0 0) :tail-pos '(0 0), :visited #{'(0, 0)}} ['(1 0) 3])
+;    (simulate ['(0 1) 2])
+;    (simulate ['(-1 0) 3]))
+
